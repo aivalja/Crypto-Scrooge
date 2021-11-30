d527e271f7b4c4f25bc4e70ad0104cbe8f1227fd
Anssi <anssi.valjakka@gmail.com>
2021-11-30 16:28:48 +0200

    volume history parsing

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import json
 from pprint import pprint
 from datetime import datetime, timezone
 
-prices = {}
 
 def main():
     #with urlopen("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY") as response:
@@ -13,11 +12,25 @@ def main():
     start_date = "01/01/2020"
     end_date = "01/08/2021"
     coin = "bitcoin"
-    price_history = get_price_history(start_date, end_date, coin)
-    price_history = parse_price_history(price_history, start_date, end_date)
+    coin_history = get_price_history(start_date, end_date, coin)
+    price_history = parse_data_history(coin_history, start_date, end_date, "prices")
+    volume_history = parse_data_history(coin_history, start_date, end_date, "total_volumes")
     print(get_longest_bear(price_history))
+    print(get_highest_volume(volume_history))
 
+def get_highest_volume(volume_history):
+    highest_volume = 0
+    highest_volume_timestamp = 0
+    for timestamp in volume_history:
+        if(volume_history[timestamp] > highest_volume):
+            highest_volume = volume_history[timestamp]
+            highest_volume_timestamp = timestamp
+    return([highest_volume, datetime.fromtimestamp(highest_volume_timestamp/1000)])
 
+def get_optimal_investment(price_history):
+    
+    for timestamp in price_history:
+        continue
 
 def get_longest_bear(price_history):
     previous_price = 0
@@ -39,28 +52,29 @@ def get_longest_bear(price_history):
         previous_price = price_history[timestamp]
     return([longest_bear_length, datetime.fromtimestamp(longest_bear_start/1000), datetime.fromtimestamp(longest_bear_end/1000)])
 
-def parse_price_history(data,start_date, end_date):
-    previous_value = data["prices"][0]
+def parse_data_history(data, start_date, end_date, data_type):
+    datapoints = {}
+    previous_value = data[data_type][0]
     expected_timestamp = int(date_to_timestamp(start_date)*1000)
     end_timestamp = int(date_to_timestamp(end_date)*1000)
     # Parse response to inlcude only values closest to midnight utc
-    for k in data["prices"]:
+    for k in data[data_type]:
         # Timestamp is correct
         if(k[0] == expected_timestamp):
-            prices[k[0]] = k[1]
+            datapoints[k[0]] = k[1]
             expected_timestamp+=86400000
         elif(k[0] > expected_timestamp):
             # Current value was closer to midnight than previous
             if(abs(k[0]-expected_timestamp)<abs(previous_value[0]-expected_timestamp)):
-                prices[k[0]] = k[1]
+                datapoints[k[0]] = k[1]
             #Previous value was closer to midnight
             else:
-                prices[previous_value[0]] = previous_value[1]
+                datapoints[previous_value[0]] = previous_value[1]
             expected_timestamp+=86400000
         if(end_timestamp < expected_timestamp):
             break
         previous_value = k
-    return prices
+    return datapoints
 
 def date_to_timestamp(date):
     dt = date.split("/")
